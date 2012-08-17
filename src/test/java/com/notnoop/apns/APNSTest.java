7dbdd99eac69a155d56a80dd2e386643c218e616
Leon Yang <yanglong76@gmail.com>
2012-08-17 17:22:00 +0800

    finished test for APNS communication
    Signed-off-by: Leon Yang <yanglong76@gmail.com>

--- src/test/java/com/notnoop/apns/APNSTest.java
+++ src/test/java/com/notnoop/apns/APNSTest.java
@@ -3,6 +3,7 @@ package com.notnoop.apns;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import com.notnoop.exceptions.NetworkIOException;
 
 /**
  * Silly Tests
@@ -23,5 +24,21 @@ public class APNSTest {
     @Test
     public void newServiceGetNewInstances() {
         assertNotSame(APNS.newService(), APNS.newService());
+    }
+
+    @Test
+    public void testUdtService() {
+        try {
+            ApnsServiceBuilder servicebuilder = new ApnsServiceBuilder();
+            ApnsService service = servicebuilder.withAppleDestination(true).withCert(ClassLoader.getSystemResource("apns_prod_ding1ding_cer.p12").getPath(), "uptops").build();
+            service.testConnection();
+            PayloadBuilder payload_builder = new PayloadBuilder();
+            payload_builder.badge(1).alertBody("This comes from Leon123");
+            String token = "1fab51a25e0d5ff1fa2033b64086d2fb76336c37cea239cf9b1ed2af3c9ab201";
+            SimpleApnsNotification msg = new SimpleApnsNotification(token, payload_builder.build());
+            service.push(msg);
+        } catch (NetworkIOException e) {
+            e.printStackTrace();
+        }
     }
 }
